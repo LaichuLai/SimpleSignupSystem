057f2d38586e62029aa84477dc8389483b1239e3
Laichu <cozzichilli@gmail.com>
2021-12-01 22:46:03 +0800

    Create Bulletin_board Project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+#GET/about
+get "about", to: "about#index"
+
+#GET/main
+root to: "main#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
