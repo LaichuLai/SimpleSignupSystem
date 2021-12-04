cc3e986c35fe2fe90b028b4c96b9865c27597e8a
Laichu <cozzichilli@gmail.com>
2021-12-04 21:43:11 +0800

    新增忘記密碼補寄功能

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,10 +7,34 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
 
     if @user.present?
+      #發送信件
       PasswordMailer.with(user: @user).reset.deliver_later
     end
     redirect_to root_path, notice: "如帳號存在，將發送密碼重設信，請至信箱確認"      
 
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "認證已失效，請重新登入"
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  
 
 end
